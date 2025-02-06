7739c89bc7f7378eb8656ee613e12b5ee0904514
Steven Wang <34365342+ShuhengW@users.noreply.github.com>
2025-02-06 20:41:18 +0000

    fix

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ get("/square_root/new") do
 end
 
 get("/square_root/results") do
-  @num = params[:number].to_f
+  @num = params[:user_number].to_f
   @result = Math.sqrt(@num)
   erb (:square_root_result)
 end
@@ -29,9 +29,27 @@ get("/payment/new") do
 end
 
 get("/payment/results") do
-  @apr = params[:apr].to_f / 100 / 12
-  @years = params[:years].to_i * 12
-  @principal = params[:principal].to_f
+  @apr_print = params[:user_apr].to_f
+  @years_print = params[:user_years].to_i
+  @principal_print = params[:user_pv].to_f
+  @apr = params[:user_apr].to_f / 100 / 12
+  @years = params[:user_years].to_i * 12
+  @principal = params[:user_pv].to_f
   @monthly_payment = (@apr * @principal) / (1- (1 + @apr)**-@years)
   erb (:payment_result)
+end
+
+get("/random/new") do
+
+  erb(:random_form)
+
+end
+
+get("/random/results") do
+
+  @min_num = params[:user_min].to_f
+  @max_num = params[:user_max].to_f
+
+  @random_number = rand(@min_num..@max_num)
+  erb(:random_result)
 end
